c3a31eedfc15084470e1d70706de476b5cfbad34
dheep <dheep@DHEEPIKA>
2024-11-08 12:31:01 +0530

    sixth commit

---
+++ C2TC_DHEEPIKA/src/com/tns/ifet/dayseven/overriding/HDFC.java
@@ -0,0 +1,10 @@
+package com.tns.ifet.dayseven.overriding;
+
+//subclass
+public class HDFC extends RBI {
+	@Override
+	public float getRateOfInterest() {
+		return 6.8f;
+	}
+
+}
---
+++ C2TC_DHEEPIKA/src/com/tns/ifet/dayseven/overriding/RBI.java
@@ -0,0 +1,10 @@
+package com.tns.ifet.dayseven.overriding;
+
+
+//Superclass
+public class RBI {
+	public float getRateOfInterest() {
+		return 6.7f;
+	}
+
+}
