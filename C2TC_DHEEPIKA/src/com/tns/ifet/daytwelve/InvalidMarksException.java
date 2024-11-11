7bd4ab2b847e28d8c97017f8828b4e2eca12cbb9
dheep <dheep@DHEEPIKA>
2024-11-11 21:29:59 +0530

    commit

---
+++ C2TC_DHEEPIKA/src/com/tns/ifet/daytwelve/InvalidMarksException.java
@@ -0,0 +1,15 @@
+package com.tns.ifet.daytwelve;
+
+
+public class InvalidMarksException extends Exception{
+
+	public InvalidMarksException() {
+		super();
+	}
+
+	public InvalidMarksException(String message) {
+		super(message);
+	}
+
+	
+}
---
+++ C2TC_DHEEPIKA/src/com/tns/ifet/daytwelve/UncheckedException.java
@@ -0,0 +1,14 @@
+package com.tns.ifet.daytwelve;
+
+
+public class UncheckedException {
+	public static void main(String[] args) {
+		int intArray[];
+		//try {
+			intArray = new int[] { 1, 2, 3, 4 };
+			System.out.println(intArray[4]);
+		//} catch (ArrayIndexOutOfBoundsException e) {
+			//System.err.println("Specified index does not exist. "+e.getMessage());
+		//}
+	}
+}
